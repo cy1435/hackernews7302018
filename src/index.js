76ca2b844c512b826fd7ca0c59147d670d3d8707
Colin Yao <colin@fo4sale.com>
2018-07-30 16:08:30 -0700

    6/28 5:00pm

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
 
-if (module.hot){
+if (module.hot) {
     module.hot.accept();
 }
